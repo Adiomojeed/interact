c55582baa1a33a96d990efee847576106d4632c6
Adiomojeed <adio.mojeed@gmail.com>
2020-05-10 22:10:11 +0100

    Completed the project setup

---
+++ src/Index.jsx
@@ -0,0 +1,10 @@
+/** @format */
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+	return <h1>hello</h1>;
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
